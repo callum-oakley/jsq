149b39451349d43fba10b46453eb99d1f9fc2e1c
Callum Oakley <hello@callumoakley.net>
2025-01-06 19:03:18 +0000

    tag on publish

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -2,17 +2,13 @@
 
 cargo install fn
 
-version=$(
-    cat Cargo.toml | fn '$.match(/version = "(\d+\.\d+\.\d+)"/)[1]'
-)
-
-newest_version=$(
-    curl https://crates.io/api/v1/crates/fn | fn -p '$.crate.newest_version'
-)
+version=$(cat Cargo.toml | fn '$.match(/version = "(\d+\.\d+\.\d+)"/)[1]')
+newest_version=$(curl https://crates.io/api/v1/crates/fn | fn -p '$.crate.newest_version')
 
 if [ "${version}" = "${newest_version}" ]; then
-  echo "Already published ${version}."
-  exit
+    echo "Already published ${version}."
+    exit
 fi
 
+gh release create "v${version}" --generate-notes
 cargo publish
