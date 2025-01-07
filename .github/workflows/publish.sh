509488c9bdb3c8d061b3ed80cb2b12136fcab06e
Callum Oakley <hello@callumoakley.net>
2025-01-07 18:27:50 +0000

    check all versions for already published not just the latest

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -3,9 +3,13 @@
 cargo install fn
 
 version=$(cat Cargo.toml | fn '$.match(/version = "(\d+\.\d+\.\d+)"/)[1]')
-newest_version=$(curl https://crates.io/api/v1/crates/fn | fn -p '$.crate.newest_version')
 
-if [ "${version}" = "${newest_version}" ]; then
+already_published=$(
+    curl https://crates.io/api/v1/crates/fn |
+        version="${version}" fn -p '$.versions.map(a => a.num).includes($version)'
+)
+
+if [ "${already_published}" = "true" ]; then
     echo "Already published ${version}."
     exit
 fi
